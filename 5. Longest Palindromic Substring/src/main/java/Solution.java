ec5013e3f89e90d0f64491ca11f095533c77c959
songpengcheng <songpengcheng@le.com>
2016-12-18 15:43:32 +0800

    5. Longest Palindromic Substring

---
+++ 5. Longest Palindromic Substring/src/main/java/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * Created by songpengcheng on 2016/12/18.
+ */
+public class Solution {
+
+    private static int lo, maxLen;
+
+    public static String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2)
+            return s;
+
+        for (int i = 0; i < len - 1; i++) {
+            extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
+            extendPalindrome(s, i, i + 1); //assume even length.
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+
+    /**
+     * 由于是方法值传递，只能把lo maxLen作为公用变量
+     */
+    private static void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("1234566743573676586759869879"));
+    }
+
+}
